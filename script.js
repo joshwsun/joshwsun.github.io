e91665b0f9211d36de0d899e346384868fb56c52
Joshua Sun <joshua.sun@colorado.edu>
2024-02-06 21:28:44 -0700

    new quotes

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ const quote_strings = [
   ["... even if you can’t ever make it off the ground, say you are a turkey—well, that is an interesting thing.", "Mary Ruefle"],
   ["It’s not what the world holds for you, it’s what you bring to it.", "L.M. Montgomery"],
   ["It is not down on any map; true places never are.", "Herman Melville"],
-  // ["And though I thought I'd done the damage I was born for, / there was still so much to step through, / so much to mar.", "Julie Carr"]
-  ["It is not down on any map; true places never are.", "Herman Melville"],
   ['"When do we start feeling like the world belongs to us?" ... "I don\'t know," I said. "Tomorrow."', "Benjamin Alire Sáenz"],
   ["But the wind always swept my words away like cloud shadows, as if it mattered more that I said them, than who heard them.", "Lauren Wolk"],
   ["Your confetti tongue explodes / into acid jazz.", "Wendy Xu"],
   ["And though I thought I'd done the damage I was born for, / there was still so much to step through, / so much to mar.", "Julie Carr"],
   ["Invisible fish swim this ghost ocean now described by waves of sand, by water-worn rock.", "Joy Harjo"],
   ["Time dripped from the faucet like a magician’s botched trick.", "Chen Chen"],
-  ["The first track still almost swings. High hat and snare, even / A few bars of sax the stratosphere will singe-out soon enough.", "Tracy K. Smith"]
+  ["The first track still almost swings. High hat and snare, even / A few bars of sax the stratosphere will singe-out soon enough.", "Tracy K. Smith"],
+  ["It snuck up on me—growing up, I mean.","Jenny Han"],
+  ["Birds would overthrow the cathedral towers. / I would have a magician’s hair, full of sleeves & saws", "Chen Chen"],
+  ["\"No more trying to be something that I'm just—just not.\" \"What if you're trying to be somebody you are?\"", "Legally Blonde (2001)"],
+  ["far west of this here and now / awash with these azealas' purples, / pinks, and whites, these late camellia reds.", "Reginald Shepherd"],
+  ["Sailing toward dawn, and I was perched atop the crow's nest, being the ship's eyes.", "Kenneth Oppel"]
 ];
 
 // Fisher-Yates Sorting Algorithm
