32bc4034c95cdb8a70600306801c603ed36fe57f
Joshua Sun <joshua.sun@colorado.edu>
2024-02-06 19:14:30 -0700

    projects added

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
+// list of quote, author
 const quote_strings = [
-  ["Of course you may be a lark, and that would be lucky. But in general pay attention to the wings, not to the sky.", "Mary Ruefle"],
+  ["... even if you can’t ever make it off the ground, say you are a turkey—well, that is an interesting thing.", "Mary Ruefle"],
   ["It’s not what the world holds for you, it’s what you bring to it.", "L.M. Montgomery"],
   ["It is not down on any map; true places never are.", "Herman Melville"],
   // ["And though I thought I'd done the damage I was born for, / there was still so much to step through, / so much to mar.", "Julie Carr"]
   ["It is not down on any map; true places never are.", "Herman Melville"],
   ['"When do we start feeling like the world belongs to us?" ... "I don\'t know," I said. "Tomorrow."', "Benjamin Alire Sáenz"],
+  ["But the wind always swept my words away like cloud shadows, as if it mattered more that I said them, than who heard them.", "Lauren Wolk"],
+  ["Your confetti tongue explodes / into acid jazz.", "Wendy Xu"],
+  ["And though I thought I'd done the damage I was born for, / there was still so much to step through, / so much to mar.", "Julie Carr"],
+  ["Invisible fish swim this ghost ocean now described by waves of sand, by water-worn rock.", "Joy Harjo"],
+  ["Time dripped from the faucet like a magician’s botched trick.", "Chen Chen"],
+  ["The first track still almost swings. High hat and snare, even / A few bars of sax the stratosphere will singe-out soon enough.", "Tracy K. Smith"]
 ];
 
 // Fisher-Yates Sorting Algorithm
+// Will randomly shuffle an array
 function shuffle(arr) { 
   for (let i = arr.length - 1; i > 0; i--) { 
     const j = Math.floor(Math.random() * (i + 1)); 
-    [arr[i], arr[j]] = [arr[j], arr[i]]; 
+    [arr[i], arr[j]] = [arr[j], arr[i]]; // swap elements
   } 
   return arr; 
 }; 
@@ -28,6 +36,14 @@ function getQuote(){
 	currentIndex = (currentIndex + 1) % shuffled_quote.length;
     document.getElementById("quote").innerHTML = shuffled_quote[currentIndex][0] + " -" + shuffled_quote[currentIndex][1];
     // document.getElementById("by").innerHTML = shuffled_quote[currentIndex][1];
+}
+
+function showDiv(divID) {
+	if (document.getElementById(divID).style.display == ""){ // hide if displayed
+		document.getElementById(divID).style.display = "none";
+	} else {
+		document.getElementById(divID).style.display = "";
+	}
 }
 
 window.onload = getQuote;
